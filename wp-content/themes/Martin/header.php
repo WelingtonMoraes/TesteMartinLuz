b4d1435b4f064fb4dc586d283fa557394cdf0094
WelingtonMoraes <welingtonfernando282@gmail.com>
2021-03-12 02:56:19 -0300

    ajuste de pastas

--- wp-content/themes/Martin/header.php
+++ wp-content/themes/Martin/header.php
@@ -12,13 +12,12 @@
     <!-- Fontes -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
 
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <!-- ////// -->
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
     <script src="<?php get_template_directory_uri() ?>/assets/package/js/swiper.min.js"></script>
-    <title>Juarez</title>
+    <title>Martin Luz</title>
   </head>
 
   <body> 
--- wp-content/themes/Martin/index.php
+++ wp-content/themes/Martin/index.php
@@ -1,5 +1,6 @@
 <?php
-/**
+/*
+*
 * Template Name: home
 *
 * @package WordPress
@@ -13,12 +14,24 @@
         <div class="col-6">
             <h1>Reembolsos corporativos de forma simples e rápida. <b>Como deve ser.</b></h1>
             <p>Acabe com a dor de cabeça da gestão de despesas do dia a dia da sua empresa: pagamentos, reembolsos, compras on-line e gestão do fundo fixo. Controle seus gastos escaneando notas fiscais, acompanhando aprovações, visualizando relatórios e muito mais!</p>
-            <a>Peça já sua proposta</a>
+            <a href="#" class="btn__claro">Peça já sua proposta</a>
             <p>Responderemos <b>em até 24h.</b></p>
         </div>
         <div class="col-6">
-            <img src="assets/images/home/img-1.png"/>
+            <img src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/images/home/img1.png"/>
         </div>
+
+        <div class="col-6">
+            <img class="home__reembolso--backimg" src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/images/home/backimg.png"/>
+        </div>
+        <div class="col-6">
+            <h2>Assuma agora o <br><b>controle dos gastos</b> variáveis da sua empresa</h2>
+            <p>O BPP Corp é uma solução única para a administração de gastos corporativos que conta com uma conta digital empresarial grátis, cartões VISA pré-pagos para controle total das despesas e um aplicativo sem custos adicionais para tornar seu controle financeiro mais simples e prático! </p>
+        </div>
+    </div>
+
+    <div class="home__organizar flex flex-wrap">
+        <h2>Assuma agora o <br><b>controle dos gastos</b> variáveis da sua empresa</h2>
     </div>
 </div>
 
