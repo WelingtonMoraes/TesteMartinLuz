7300b07072481107634f825fb6db9f027fd510dc
WelingtonMoraes <welingtonfernando282@gmail.com>
2021-03-13 00:22:22 -0300

    modal de videos

--- wp-content/themes/Martin/assets/js/slides.js
+++ wp-content/themes/Martin/assets/js/slides.js
@@ -1,11 +1,9 @@
 $(function(){
 
-	var energia = new Swiper('.swiper-container.energia_content', {
+	var reduzir = new Swiper('.swiper-container.home__reduzir--icons---swiper', {
     slidesPerView: 1,
-    spaceBetween: 60,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
+    slidesPerColumn: 1,
+    spaceBetween: 30,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
@@ -18,136 +16,28 @@ $(function(){
     breakpoints: {
       768: {
         slidesPerView: 3,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
+        slidesPerColumn: 2,
+        spaceBetween: 0,
       }
-    }
-  });
-
-  var swiper = new Swiper('.swiper-container.info_swiper', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-
-  $('#modal-contato').on('click', function () {
-    var modalconta = document.getElementById('mov-modal');
-    if(modalconta.style.right == '0px'){
-      $('#mov-modal').css('right', '-510px');
-      document.getElementById('iconModal').classList.remove('fa-times');
-      document.getElementById('iconModal').classList.add('fa-arrow-left');
-    }else{
-      $('#mov-modal').css('right', '0px');
-      document.getElementById('iconModal').classList.remove('fa-arrow-left');
-      document.getElementById('iconModal').classList.add('fa-times');
     }
   });
 })
 
-window.onscroll = function() {FunctionScroll()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function FunctionScroll() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+  var modal = document.getElementById("playvideo");
+  var btn = document.getElementById("playV");
+  var spanClose = document.getElementsByClassName('close');
+  btn.onclick = function() {
+      modal.style.display = "block";
+	  $('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
   }
-}
-
-function openNav() {
-  document.getElementById("mySidepanel").style.width = "100%";
-}
-
-function closeNav() {
-  document.getElementById("mySidepanel").style.width = "0";
-}
-
-function addZero(n){
-	
-	if(n < 10){
-		return `0${n}`;
-	}
-	
-	return n;
-}
-
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
+  function closeVideo() {
+    modal.style.display = "none";
+    $('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      $('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
     }
-  });
-}
-
-    var modal1 = document.getElementById("playvideo1");
-    var btn1 = document.getElementById("playV1");
-    var span1 = document.getElementsByClassName("close")[0];
-    btn1.onclick = function() {
-        modal1.style.display = "block";
-
-	    $('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
-    }
-    span1.onclick = function() {
-        modal1.style.display = "none";
-        $('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
-    }
-    window.onclick = function(event) {
-        if (event.target == modal1) {
-            modal1.style.display = "none";
-            $('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
-        }
-    }
-
-
-    var modal2 = document.getElementById("playvideo2");
-    var btn2 = document.getElementById("playV2");
-    var span2 = document.getElementsByClassName("close")[1];
-    btn2.onclick = function() {
-        modal2.style.display = "block";
-
-	    $('.youtube-video')[1].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
-    }
-    span2.onclick = function() {
-        modal2.style.display = "none";
-        $('.youtube-video')[1].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
-    }
-    window.onclick = function(event) {
-        if (event.target == modal2) {
-            modal2.style.display = "none";
-            $('.youtube-video')[1].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
-        }
-    }
-
-
-    var modal3 = document.getElementById("playvideo3");
-    var btn3 = document.getElementById("playV3");
-    var span3 = document.getElementsByClassName("close")[2];
-    btn3.onclick = function() {
-        modal3.style.display = "block";
-
-	    $('.youtube-video')[2].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
-    }
-    span3.onclick = function() {
-        modal3.style.display = "none";
-        $('.youtube-video')[2].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
-    }
-    window.onclick = function(event) {
-        if (event.target == modal3) {
-            modal3.style.display = "none";
-            $('.youtube-video')[2].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
-        }
-    }
+  }
     
--- wp-content/themes/Martin/index.php
+++ wp-content/themes/Martin/index.php
@@ -15,14 +15,26 @@
             <h1>Reembolsos corporativos de forma simples e rápida. <b>Como deve ser.</b></h1>
             <p>Acabe com a dor de cabeça da gestão de despesas do dia a dia da sua empresa: pagamentos, reembolsos, compras on-line e gestão do fundo fixo. Controle seus gastos escaneando notas fiscais, acompanhando aprovações, visualizando relatórios e muito mais!</p>
             <a href="#" class="btn__claro">Peça já sua proposta</a>
-            <p>Responderemos <b>em até 24h.</b></p>
+            <p style="color: #0285C9;" >Responderemos <b>em até 24h.</b></p>
         </div>
         <div class="col-6">
             <img src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/images/home/img1.png"/>
         </div>
 
         <div class="col-6">
-            <img class="home__reembolso--backimg" src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/images/home/backimg.png"/>
+            <a href="javascript:void(0)" id="playV">
+                <img class="home__reembolso--backimg" src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/images/home/backimg.png"/>
+            </a>
+            <div id="playvideo" class="modalVideo">
+                <div class="modal-content">
+                    <div class="">
+                        <span onclick="closeVideo()" class="close">&times;</span>
+                    </div>
+                    <div class="modal-body">
+                        <iframe class="youtube-video" width="100%" height="100%" src="https://www.youtube.com/embed/4oeVCWTyUpY?enablejsapi=1&version=3&playerapiid=ytplayer" frameborder="0" style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="col-6">
             <h2>Assuma agora o <br><b>controle dos gastos</b> variáveis da sua empresa</h2>
@@ -31,7 +43,98 @@
     </div>
 
     <div class="home__organizar flex flex-wrap">
-        <h2>Assuma agora o <br><b>controle dos gastos</b> variáveis da sua empresa</h2>
+        <div class="col-6">
+            <div class="home__organizar--text">
+                <h2><b>Organizar reembolsos</b> da sua empresa nunca foi tão rápido</h2>
+                <p>Utilizando BPP Corp, você tem controle total sobre o budget de sua equipe podendo realizar transferências instantâneas para os cartões corporativos dos colaboradores, acompanhar gastos, receber comprovantes e visualizar relatórios que organizam seu reembolso.</p>
+                <a href="#" class="btn__escuro">Peça já sua proposta</a>
+            </div>
+        </div>
+        <div class="col-6 home__organizar--img">
+            <img src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/images/home/bpp_corp_reembolso.png"/>
+        </div>
+    </div>
+
+    <div class="home__reduzir flex flex-wrap">
+        <div class="home__reduzir--text col-12">
+            <h1>A plataforma que ajuda a <b>reduzir os gastos</b> do dia a dia</h1>
+            <p>Simplifique a gestão das despesas, evitando a perda de tempo com comprovantes e planilhas, reduzindo custos e aumentando os resultados da sua empresa.</p>
+        </div>
+        <div class="home__reduzir--icons">
+            <div class="swiper-container home__reduzir--icons---swiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="content-box">
+                            <img src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/icons/home/iconReduzir1.svg"/>
+                            <h3>Mais rapidez na gestão das suas despesas</h3>
+                            <p>Para que seu negócio cresça de maneira inteligente e rápida, substitua os processos demorados de reembolsos, aprovações, criação de relatórios e etc. por uma plataforma que te oferece tudo isso de forma rápida e digital. </p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="content-box">
+                            <img src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/icons/home/iconReduzir4.svg"/>
+                            <h3>Apresente suas despesas com um só clique</h3>
+                            <p>Enviar seus comprovantes é mais simples do que você imagina. Basta que o colaborador tire uma foto do cupom fiscal com seu smartphone através do aplicativo BPP Card. O gestor das despesas pode aprovar ou reprovar aquele gasto através do App.</p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="content-box">
+                            <img src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/icons/home/iconReduzir2.svg"/>
+                            <h3>Os relatórios que você precisa em um único lugar</h3>
+                            <p>Encontre relatórios detalhados sobre as utilizações de cada cartão corporativo cadastrado em sua equipe. Confira o saldo de cada cartão, redistribua valores, faça transferências instantâneas e não perca tempo com formulários e planilhas. </p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="content-box">
+                            <img src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/icons/home/iconReduzir5.svg"/>
+                            <h3>Controle seus gastos com cartões pré-pagos VISA</h3>
+                            <p>Os cartões pré-pago BPP Corp são fáceis de controlar: o colaborador só gasta o que a empresa disponibiliza para compras online ou em lojas físicas. Conte com cartões VISA aceitos em mais de 30 milhões de estabelecimentos no mundo todo, incluindo sites e aplicativos!</p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="content-box">
+                            <img src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/icons/home/iconReduzir3.svg"/>
+                            <h3>Organize todos os seus comprovantes</h3>
+                            <p>Recebendo os comprovantes de gastos de seus colaboradores através da plataforma, você não precisa mais guardar comprovantes e notas fiscais em lugar nenhum. Os gastos podem ser classificados por tipo ou por projeto, da forma que você precisar.</p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="content-box">
+                            <img src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/icons/home/iconReduzir6.svg"/>
+                            <h3>Conta digital gratuita e exclusiva para sua empresa</h3>
+                            <p>Oferecemos uma conta digital gratuita especialmente para gerenciar despesas.  Transfira o saldo de sua conta para os cartões de maneira instantânea, sempre livre de taxas de manutenção ou de juros.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Add Pagination -->
+                <div class="swiper-pagination"></div>
+            </div>
+        </div>
+    </div>
+    <div class="home__vantagens flex flex-wrap">
+        <div class="col-6 flex flex-wrap">
+            <div class="home__vantagens--box col-6 alignVertical">
+                <img src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/images/home/BK.png"/>
+            </div>
+            <div class="home__vantagens--box col-6 alignVertical">
+                <img src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/images/home/boticario.png"/>
+            </div>
+            <div class="home__vantagens--box col-6 alignVertical">
+                <img src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/images/home/cacau.png"/>
+            </div>
+            <div class="home__vantagens--box col-6 alignVertical">
+                <img src="<?php get_template_directory_uri() ?>/wp-content/themes/Martin/assets/images/home/pague.png"/>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="home__vantagens--text col-6">
+                <h1>Conheça quem já conta com as <b>vantagens excluisivas</b> do BPP Corp</h1>
+                <p>A empresas mais bem-sucedidas do mundo, grandes e pequenas, confiam no BPP Corp para reduzir custos com organização dos gastos da suas empresas. </p>
+            </div>
+        </div>
+    </div>
+    <div class="home__vantagens flex flex-wrap">
+        
     </div>
 </div>
 
